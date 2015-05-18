80ad2e69a8885e789c0c3c17ee2d597e6d1d6608
MasahiroSakoda <thunderbird.m.s@gmail.com>
2015-05-18 10:32:42 +0900

    add action to login into home/index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
+    let (:user) {build(:user)}
+
+    before do
+      login_user user
+    end
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
